9e25c03fe8aecadecebc463553d7081179a093e1
Artem Strelnikov <piramida1237@gmail.com>
2025-05-20 22:00:49 +0200

    20.05-2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,15 +3,26 @@ import Button from "../Button/Button";
 import Alert from "../Alert/Alert";
 import UserMenu from "../UserMenu/UserMenu";
 import { useState } from "react";
+import ClickCounter from "../ClickCounter";
+
+interface Values {
+  x: number;
+  y: number;
+}
 
 export default function App() {
-  const handleClick = () => {
-    console.log("I'm a button!");
+  const [values, setValues] = useState<Values>({ x: 0, y: 0 });
+
+  const updateValue = (key: keyof Values) => {
+    setValues({
+      ...values,
+      [key]: values[key] + 1,
+    });
   };
 
   const [clicks, setClicks] = useState(0);
 
-  const handleClickThree = () => {
+  const handleClick = () => {
     setClicks(clicks + 1);
   };
 
@@ -37,8 +48,15 @@ export default function App() {
       <>
         <Button variant="primary" text="Login" />
         <Button variant="secondary" text="Follow" />
-        <button onClick={handleClick}>Click me!</button>;
-        <button onClick={handleClickThree}>Current: {clicks}</button>;
+        <ClickCounter value={clicks} onUpdate={handleClick} />
+        <ClickCounter value={clicks} onUpdate={handleClick} />
+      </>
+      <>
+        <p>
+          x: {values.x}, y: {values.y}
+        </p>
+        <button onClick={() => updateValue("x")}>Update x</button>
+        <button onClick={() => updateValue("y")}>Update y</button>
       </>
     </>
   );
---
+++ src/components/ClickCounter.tsx
@@ -0,0 +1,8 @@
+interface ClickCounterProps {
+  value: number;
+  onUpdate: () => void;
+}
+
+export default function ClickCounter({ value, onUpdate }: ClickCounterProps) {
+  return <button onClick={onUpdate}>Clicked: {value}</button>;
+}
