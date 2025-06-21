92611a40546078e17c51c3fa6072d0571acf4f1d
Artem Strelnikov <piramida1237@gmail.com>
2025-06-21 22:02:45 +0200

    pol

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -54,15 +54,3 @@ export default function App() {
     </>
   );
 }
-
-function checkStorage(available, ordered) {
-  if (ordered <= 0) {
-    return `There are no products in the order!`;
-  } else if (available < ordered) {
-    return `Your order is too large, there are not enough items in stock!`;
-  } else {
-    return `The order is accepted, our manager will contact you`;
-  }
-}
-
-console.log(checkStorage(100, 0));
