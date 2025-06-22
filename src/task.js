4952a8bcdb30960e59a72f200421c4fefe7ccf73
Artem Strelnikov <piramida1237@gmail.com>
2025-06-22 11:52:38 +0200

    jgh

--- src/task.js
+++ src/task.js
@@ -4,3 +4,4 @@ console.log(a > 10 && a < 30); // true && true -> true
 const b = 50;
 console.log(b > 10 && b < 30); // true && false -> false
 console.log(b > 80 && b < 120); // false && true -> false
+console.log(null && true);
