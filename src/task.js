8579c8eb70980a039909009e17c6d2692e73b2df
Artem Strelnikov <piramida1237@gmail.com>
2025-06-22 11:45:22 +0200

    true

--- src/task.js
+++ src/task.js
@@ -1,3 +1,6 @@
-const number = 10;
+const a = 20;
+console.log(a > 10 && a < 30); // true && true -> true
 
-console.log(Boolean(number));
+const b = 50;
+console.log(b > 10 && b < 30); // true && false -> false
+console.log(b > 80 && b < 120); // false && true -> false
