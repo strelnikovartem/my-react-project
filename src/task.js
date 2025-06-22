05bc40940dc91e4354502bbe53d56899dd915fa2
Artem Strelnikov <piramida1237@gmail.com>
2025-06-22 08:27:24 +0200

    func

--- src/task.js
+++ src/task.js
@@ -1,6 +1,7 @@
-const a = 15;
-const b = 10;
+function getBiggerNumber(a, b) {
+  return a > b ? a : b;
+}
 
-const bigNumber = a > b ? "first" : "two";
-
-console.log(bigNumber);
+console.log(getBiggerNumber(5, 10)); // 10
+console.log(getBiggerNumber(20, 15)); // 20
+console.log(getBiggerNumber(7, 7)); // 7
