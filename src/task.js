b924b73d0ac80394eaaed71b76d35510b770e967
Artem Strelnikov <piramida1237@gmail.com>
2025-06-25 21:08:50 +0200

    rtf

--- src/task.js
+++ src/task.js
@@ -1,3 +1,13 @@
+function calculateTotal(number) {
+  let sum = 0;
+  for (let index = 0; index <= number; index += 1) {
+    sum += index;
+  }
+  return sum;
+}
+
+console.log(calculateTotal(7));
+
 function sumUpTo(number) {
   let sum = 0;
 
@@ -9,6 +19,6 @@ function sumUpTo(number) {
   return sum;
 }
 
-console.log(sumUpTo(5)); // 15
+// console.log(sumUpTo(5)); // 15
 // console.log(sumUpTo(10)); // 55
 // console.log(sumUpTo(0)); // 0
