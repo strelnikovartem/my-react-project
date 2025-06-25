f3dc2ed77716ca2a9faab33f97aef84f56fb9742
Artem Strelnikov <piramida1237@gmail.com>
2025-06-25 21:51:06 +0200

    decv

--- src/task.js
+++ src/task.js
@@ -1,17 +1,10 @@
-function findNumber(max, target) {
-  console.log("Log in the body of the function before the cycle");
-
-  for (let i = 5; i <= max; i += 1) {
-    console.log("Current counter value i:", i);
-
-    if (i === target) {
-      console.log(`Found the number $ {target}, interrupt the cycle`);
-      break;
+function findNumber(start, end, divisor) {
+  for (let i = start; i < end; i++) {
+    if (i % divisor === 0) {
+      return i;
     }
   }
-
-  console.log("Log in body function after cycle");
 }
 
-findNumber(10, 6);
-console.log("Log after exiting function");
+console.log(findNumber(2, 6, 5));
+console.log(findNumber(16, 35, 7));
