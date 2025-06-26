3da0e83cfe05a5d87da5d6971b0c290059cad5a8
Artem Strelnikov <piramida1237@gmail.com>
2025-06-26 21:20:27 +0200

    task-1.[}

--- src/task.js
+++ src/task.js
@@ -1,4 +1,9 @@
-const planets = ["Earth", "Mars", "Venus"];
-const lastElementIndex = planets.length - 1;
-console.log(planets[lastElementIndex]); // "Venus"
-console.log(lastElementIndex);
+function getLastElementMeta(array) {
+  // const index = array.length;
+  // console.log(index);
+  return [array.length - 1, array[array.length - 1]];
+}
+
+console.log(getLastElementMeta(["apple", "peach", "pear", "banana"]));
+console.log(getLastElementMeta(["apple", "peach", "pear"]));
+console.log(getLastElementMeta(["apple"]));
