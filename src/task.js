0f11a99655878cfc84801f0769a941fd40996f47
Artem Strelnikov <piramida1237@gmail.com>
2025-06-26 21:02:39 +0200

    dvf

--- src/task.js
+++ src/task.js
@@ -1,10 +1,3 @@
-function findNumber(start, end, divisor) {
-  for (let i = start; i < end; i++) {
-    if (i % divisor === 0) {
-      return i;
-    }
-  }
-}
-
-console.log(findNumber(2, 6, 5));
-console.log(findNumber(16, 35, 7));
+const planets = ["Earth", "Mars", "Venus"];
+const firstElement = planets[0];
+console.log(firstElement); // 'Earth'
