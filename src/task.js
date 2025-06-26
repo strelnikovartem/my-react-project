2b01644e81bfedb5332880cc606c8c1c1dd18ed0
Artem Strelnikov <piramida1237@gmail.com>
2025-06-26 21:20:51 +0200

    ggj

--- src/task.js
+++
@@ -1,9 +0,0 @@
-function getLastElementMeta(array) {
-  // const index = array.length;
-  // console.log(index);
-  return [array.length - 1, array[array.length - 1]];
-}
-
-console.log(getLastElementMeta(["apple", "peach", "pear", "banana"]));
-console.log(getLastElementMeta(["apple", "peach", "pear"]));
-console.log(getLastElementMeta(["apple"]));
