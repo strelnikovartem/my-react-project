b3e92a821a7de2788cce0478ec274a137ff791c0
Artem Strelnikov <piramida1237@gmail.com>
2025-06-27 19:24:58 +0200

    ffuck

---
+++ src/task.js
@@ -0,0 +1,3 @@
+const planets = ["Earth", "Mars", "Venus", "Jupiter", "Saturn"];
+console.log(planets.slice(-2)); // ["Mars", "Venus", "Jupiter", "Saturn"]
+console.log(planets.slice(2)); // ["Venus", "Jupiter", "Saturn"]
