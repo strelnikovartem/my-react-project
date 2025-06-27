e53d1d0716bee36c99837f0a11e0fef21b4c48fa
Artem Strelnikov <piramida1237@gmail.com>
2025-06-27 19:15:20 +0200

    dcfv

--- src/task.js
+++ src/task.js
@@ -1,4 +1,4 @@
 const planets = ["Earth", "Mars", "Venus", "Jupiter", "Saturn"];
 console.log(planets.slice(0, 2)); // ['Earth', 'Mars']
-console.log(planets.slice(3, 4)); // ['Earth', 'Mars', 'Venus', 'Jupiter']
+console.log(planets.slice(1, 4)); // ['Earth', 'Mars', 'Venus', 'Jupiter']
 console.log(planets.slice(1, 3)); // ['Mars', 'Venus']
