206e4c55bda728d987f924a08c8bd576e5855b0f
Artem Strelnikov <piramida1237@gmail.com>
2025-06-27 18:54:46 +0200

    sxsd

--- src/task.js
+++ src/task.js
@@ -1,8 +1,6 @@
-function transformString(string) {
-  const words = string.split(" ");
-  console.log(words);
-  return words.join("-");
+function getLength(array) {
+  const stringArray = array.join("");
+  return stringArray.length;
 }
 
-console.log(transformString("user_age")); // "user-age"
-transformString("price_per_droid"); // "price-per-droid"
+console.log(getLength(["Mango", "hurries", "to", "the", "train"]));
