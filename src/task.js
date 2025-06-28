b18c1608175c14eee1416d372af3d0b1dd7014d2
Artem Strelnikov <piramida1237@gmail.com>
2025-06-28 20:56:41 +0200

    dedfd

--- src/task.js
+++ src/task.js
@@ -1,7 +1,13 @@
-const fruits = ["apple", "banana", "orange"];
-
-if (fruits.includes("banana")) {
-  console.log("The array has an element banana");
-} else {
-  console.log("Array does not contain banana element");
+function checkStorage(storage, item) {
+  const normItem = item.toLowerCase();
+  for (let i = 0; i < storage.length; i++) {
+    if (storage.includes(normItem)) {
+      return `${normItem} is available to order!`;
+    }
+    return `Sorry! We are out of stock!`;
+  }
 }
+
+console.log(checkStorage(["apple", "plum", "pear"], "pLum"));
+console.log(checkStorage(["apple", "plum", "pear"], "orange"));
+console.log(checkStorage(["apple", "plum", "pear"], "par"));
