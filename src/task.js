06117e232f0cde147f75bb9579eb8390e475fc5b
Artem Strelnikov <piramida1237@gmail.com>
2025-06-28 22:30:00 +0200

    sdfg

--- src/task.js
+++ src/task.js
@@ -1,14 +1,15 @@
-function getCommonElements(array1, array2) {
-  const array3 = [];
-  for (let i = 0; i <= array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      array3.push(array1[i]);
-    }
-  }
-  return array3;
+const planets = ["Earth", "Mars", "Venus"];
+
+for (const planet of planets) {
+  console.log(planet);
 }
 
-console.log(getCommonElements([1, 2, 3], [2, 4]));
-console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
-console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
-console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+function calculateTotalPrice(order) {
+  let sum = 0;
+  for (const number of order) {
+    sum += number;
+  }
+  return sum;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
