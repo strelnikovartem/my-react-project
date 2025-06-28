21f533fb30d1572f8ec4c7226bf8273f7c49dc40
Artem Strelnikov <piramida1237@gmail.com>
2025-06-28 18:39:09 +0200

    cdcvc

--- src/task.js
+++ src/task.js
@@ -1,33 +1,5 @@
-function getSlice(array, value) {
-  const index = array.indexOf(value);
+const planets = ["Earth", "Mars", "Venus"];
 
-  if (array.indexOf(value) === -1) {
-    return [];
-  } else {
-    return array.slice(0, index + 1);
-  }
+for (let i = 0; i < planets.length; i += 1) {
+  console.log(planets[i]);
 }
-
-// console.log(getSlice(["Mango", "Poly", "Ajax"], "Poly"));
-// console.log(getSlice(["Mango", "Poly", "Ajax"], "Ajax"));
-// console.log(getSlice(["Mango", "Poly", "Ajax"], "Jacob"));
-
-const tags = [];
-
-for (let i = 0; i < 3; i += 1) {
-  tags.push(`tag-${i}`);
-}
-
-// console.log(tags);
-
-function createArrayOfNumbers(min, max) {
-  const number = [];
-  for (let index = min; index <= max; index++) {
-    number.push(index);
-  }
-  return number;
-}
-
-console.log(createArrayOfNumbers(1, 3));
-console.log(createArrayOfNumbers(14, 17));
-console.log(createArrayOfNumbers(29, 34));
