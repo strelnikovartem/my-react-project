c851c9f97fb24340b0cd6bcbdc2a754ca60861a9
Artem Strelnikov <piramida1237@gmail.com>
2025-06-28 22:24:18 +0200

    xzxs

--- src/task.js
+++ src/task.js
@@ -2,17 +2,13 @@ function getCommonElements(array1, array2) {
   const array3 = [];
   for (let i = 0; i <= array1.length; i++) {
     if (array2.includes(array1[i])) {
-      array3.push(i);
+      array3.push(array1[i]);
     }
-    return array3;
   }
+  return array3;
 }
 
 console.log(getCommonElements([1, 2, 3], [2, 4]));
 console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
-
-const planets = ["Earth", "Mars", "Venus"];
-
-for (let i = 0; i < planets.length; i += 1) {
-  console.log(planets[i]);
-}
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
