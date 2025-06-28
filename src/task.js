ac5e230b37848f37cbac6576bc7fc4da5dfcf086
Artem Strelnikov <piramida1237@gmail.com>
2025-06-28 22:14:28 +0200

    cdx

--- src/task.js
+++ src/task.js
@@ -1,13 +1,18 @@
-function checkStorage(storage, item) {
-  const normItem = item.toLowerCase();
-  for (let i = 0; i < storage.length; i++) {
-    if (storage.includes(normItem)) {
-      return `${normItem} is available to order!`;
+function getCommonElements(array1, array2) {
+  const array3 = [];
+  for (let i = 0; i <= array1.length; i++) {
+    if (array2.includes(array1[i])) {
+      array3.push(i);
     }
-    return `Sorry! We are out of stock!`;
+    return array3;
   }
 }
 
-console.log(checkStorage(["apple", "plum", "pear"], "pLum"));
-console.log(checkStorage(["apple", "plum", "pear"], "orange"));
-console.log(checkStorage(["apple", "plum", "pear"], "par"));
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+
+const planets = ["Earth", "Mars", "Venus"];
+
+for (let i = 0; i < planets.length; i += 1) {
+  console.log(planets[i]);
+}
