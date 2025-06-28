1485f70de004c853cc8759627d0916529f7768d4
Artem Strelnikov <piramida1237@gmail.com>
2025-06-28 18:54:51 +0200

    fvgfvg

--- src/task.js
+++ src/task.js
@@ -1,5 +1,10 @@
-const planets = ["Earth", "Mars", "Venus"];
-
-for (let i = 0; i < planets.length; i += 1) {
-  console.log(planets[i]);
+function calculateTotalPrice(order) {
+  let sum = 0;
+  for (let index = 0; index < order.length; index++) {
+    sum += order[index];
+  }
+  return sum;
 }
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
