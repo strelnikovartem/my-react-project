19e2d47a0064dc201c1664ddce7e82074647bcc0
Artem Strelnikov <piramida1237@gmail.com>
2025-07-01 09:53:45 +0200

    dfgh

--- src/task.js
+++ src/task.js
@@ -4,9 +4,14 @@ const book = {
   genres: ["historical prose", "adventure"],
   isPublic: true,
   rating: 8.38,
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
 };
 
-console.log();
+console.log(book["owner"]);
 
 console.log(book.title); // "The Last Kingdom"
 console.log(book["title"]); // "The Last Kingdom"
@@ -15,5 +20,5 @@ console.log(book.genres); // ["historical prose", "adventure"]
 console.log(book["genres"]); // ["historical prose", "adventure"]
 
 const propKey = "author";
-console.log(book.propKey); // undefined
+console.log(book.author); // undefined
 console.log(book[propKey]); // "Bernard Cornwell"
