6b58ad1f0220bca95b28dba7691b02e147de70a8
Artem Strelnikov <piramida1237@gmail.com>
2025-07-01 08:46:47 +0200

    sxcv

--- src/task.js
+++ src/task.js
@@ -1,16 +1,15 @@
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  isPublic: true,
-  rating: 8.38,
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  location: {
+    country: "Jamaica",
+    city: "Ocho Rios",
+  },
+  hobbies: ["swimming", "music", "sci-fi"],
 };
 
-const bookTitle = book.title;
-console.log(bookTitle); // "The Last Kingdom"
+const location = user.location;
+console.log(location); // {country: "Jamaica", city: "Ocho Rios"}
 
-const bookGenres = book.genres;
-console.log(bookGenres); // ["historical prose", "adventure"]
-
-const bookPrice = book.price;
-console.log(bookPrice); // undefined
+const country = user.location.country;
+console.log(country); // "Jamaica"
