560f138a9491a512b87423f158f5a228c9761b19
Artem Strelnikov <piramida1237@gmail.com>
2025-07-01 10:03:17 +0200

    sxzd

--- src/task.js
+++ src/task.js
@@ -6,6 +6,10 @@ const book = {
   rating: 8.38,
 };
 
+console.log(book.rating); // 9
+console.log(book.isPublic); // false
+console.log(book.genres); // ["historical prose", "adventure", "drama"]
+
 book.rating = 9;
 book.isPublic = false;
 book.genres.push("drama");
