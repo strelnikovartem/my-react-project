03d1e55b5c852ffae7d393a00b19059474e4dd69
Artem Strelnikov <piramida1237@gmail.com>
2025-07-01 08:44:59 +0200

    lkjm

---
+++ src/task.js
@@ -0,0 +1,16 @@
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
+
+const bookTitle = book.title;
+console.log(bookTitle); // "The Last Kingdom"
+
+const bookGenres = book.genres;
+console.log(bookGenres); // ["historical prose", "adventure"]
+
+const bookPrice = book.price;
+console.log(bookPrice); // undefined
