87d51e524ed0a4d9ad8c983bc188355d944e70cb
Artem Strelnikov <piramida1237@gmail.com>
2025-07-01 09:03:19 +0200

    fvfg

--- src/task.js
+++ src/task.js
@@ -1,24 +1,17 @@
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-  tags: ["premium", "promoted", "top"],
-  owner: {
-    name: "Henry",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
 };
 
-const ownerName = apartment.owner.name;
-const ownerPhone = apartment.owner.phone;
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags.length;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[apartment.tags.length - 1];
+console.log(book.title); // "The Last Kingdom"
+console.log(book["title"]); // "The Last Kingdom"
 
-console.log(ownerName);
-console.log(numberOfTags);
-console.log(firstTag);
-console.log(lastTag);
+console.log(book.genres); // ["historical prose", "adventure"]
+console.log(book["genres"]); // ["historical prose", "adventure"]
+
+const propKey = "author";
+console.log(book.propKey); // undefined
+console.log(book[propKey]); // "Bernard Cornwell"
