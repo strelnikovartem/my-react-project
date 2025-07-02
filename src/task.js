39d3e7b270478277380b8861af85e99b83436128
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 09:19:03 +0200

    mnjk

--- src/task.js
+++ src/task.js
@@ -1,18 +1,11 @@
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
-
-const key = Object.keys(apartment);
-
-console.log(key);
-
 const book = {
-  title: "The Last Kingdom",
   author: "Bernard Cornwell",
   genres: ["historical prose", "adventure"],
   rating: 8.38,
 };
 const keys = Object.keys(book);
-console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+for (const key of keys) {
+  console.log(key); // Ключ
+  console.log(book[key]); // Значення властивості
+}
