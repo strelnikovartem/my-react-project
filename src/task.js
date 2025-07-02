3b1c72306fa713082d1fa9a74610e02bec8c9a8e
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 10:17:56 +0200

    ddf

--- src/task.js
+++ src/task.js
@@ -1,21 +1,10 @@
-function countProps(object) {
-  let propCount = 0;
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+console.log(keys); // ["title", "author", "rating"]
 
-  // for (const key in object) {
-  //   if (object.hasOwnProperty(key)) {
-  //     propCount += 1;
-  //   }
-  // }
-
-  const keys = Object.keys(object);
-
-  for (const key of keys) {
-    propCount += 1;
-  }
-
-  return propCount;
-}
-
-console.log(countProps({}));
-console.log(countProps({ name: "Mango", age: 2 }));
-console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+const values = Object.values(book);
+console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
