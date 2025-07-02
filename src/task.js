05421f48eda30f2c396c97a2f4cfaf975cf23707
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 11:56:52 +0200

    dcxrgb

--- src/task.js
+++ src/task.js
@@ -1,24 +1,19 @@
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
 ];
 
-for (const book of books) {
-  console.log(book); // Об'єкт книги
-  console.log(book.title); // Назва
-  console.log(book.author); // Автор
-  console.log(book.rating); // Рейтинг
+const hexColors = [];
+const rgbColors = [];
+
+const values = Object.values(colors);
+
+for (const value of values) {
+  hexColors.push(value.hex);
+  rgbColors.push(value.rgb);
 }
+
+console.log(hexColors);
+console.log(rgbColors);
