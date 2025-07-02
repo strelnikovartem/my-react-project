b9d018967a10e8b90fcdac4da8da6e5f3d1e83f0
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 10:28:50 +0200

    sdcfv

--- src/task.js
+++ src/task.js
@@ -1,16 +1,10 @@
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  rating: 8.38,
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
 };
 
-const total = 0;
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
 
-const values = Object.values(book);
-
-for (const value of values) {
-  total += 1;
-}
-
-console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
-console.log(total);
+console.log(keys);
