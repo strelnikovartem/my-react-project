c1cd9ac78d4dbfb99382630c86153a8a9b47bc14
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 09:02:37 +0200

    dcfvd

--- src/task.js
+++ src/task.js
@@ -1,11 +1,16 @@
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  rating: 8.38,
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
 };
 
-for (const key in book) {
-  console.log(key); // Ключ
-  console.log(book[key]); // Значення властивості з таким ключем
+const keys = [];
+const values = [];
+
+for (const key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
 }
+
+console.log(keys);
+console.log(values);
