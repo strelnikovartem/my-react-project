e417e774e7cefa9afdb2ecd725b3f90be4d37df4
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 12:04:16 +0200

    dddfff

--- src/task.js
+++ src/task.js
@@ -1,15 +1,33 @@
-const books = [
-  { title: "The Last Kingdom", author: "Bernard Cornwell" },
-  { title: "Beside Still Waters", author: "Robert Sheckley" },
-  { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky" },
+// const books = [
+//   { title: "The Last Kingdom", author: "Bernard Cornwell" },
+//   { title: "Beside Still Waters", author: "Robert Sheckley" },
+//   { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky" },
+// ];
+
+// const authorToSearchFor = "Robert Sheckley";
+
+// for (const book of books) {
+//   if (book.author === authorToSearchFor) {
+//     console.log(book);
+//     console.log(book.title);
+//     console.log(book.rating);
+//   }
+// }
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
 ];
 
-const authorToSearchFor = "Robert Sheckley";
+const hexColors = [];
+const rgbColors = [];
 
-for (const book of books) {
-  if (book.author === authorToSearchFor) {
-    console.log(book);
-    console.log(book.title);
-    console.log(book.rating);
-  }
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
 }
+
+console.log(hexColors);
+console.log(rgbColors);
