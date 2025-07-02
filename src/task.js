7d2ac9d57a1082174ca011fb059eeed7c4f50366
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 11:46:19 +0200

    ddcx

--- src/task.js
+++ src/task.js
@@ -1,14 +1,24 @@
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+];
 
-  const values = Object.values(salaries);
-
-  for (const value of values) {
-    totalSalary += value;
-  }
-  return totalSalary;
+for (const book of books) {
+  console.log(book); // Об'єкт книги
+  console.log(book.title); // Назва
+  console.log(book.author); // Автор
+  console.log(book.rating); // Рейтинг
 }
-
-console.log(countTotalSalary({}));
-console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
-console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
