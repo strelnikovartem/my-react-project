7e52d9d6ff39b77467db7c77a09fd8fed3429839
Artem Strelnikov <piramida1237@gmail.com>
2025-07-02 10:25:51 +0200

    dddcxd

--- src/task.js
+++ src/task.js
@@ -3,8 +3,14 @@ const book = {
   author: "Bernard Cornwell",
   rating: 8.38,
 };
-const keys = Object.keys(book);
-console.log(keys); // ["title", "author", "rating"]
+
+const total = 0;
 
 const values = Object.values(book);
+
+for (const value of values) {
+  total += 1;
+}
+
 console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
+console.log(total);
