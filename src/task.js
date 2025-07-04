2f83c663cf79a0fff67f79a0a8112ecb1f8a7ca6
Artem Strelnikov <piramida1237@gmail.com>
2025-07-04 12:00:00 +0200

    ddd

--- src/task.js
+++ src/task.js
@@ -1,36 +1,18 @@
-// function getAllPropValues(propName) {
-//   const products = [
-//     { name: "Radar", price: 1300, quantity: 4 },
-//     { name: "Scanner", price: 2700, quantity: 3 },
-//     { name: "Droid", price: 400, quantity: 7 },
-//     { name: "Grip", price: 1200, quantity: 9 },
-//   ];
-
-//   const array = [];
-
-//   for (const product of products) {
-//     array.push(product[propName]);
-//   }
-//   return array;
-// }
-
-// console.log(getAllPropValues("name"));
-// console.log(getAllPropValues("quantity"));
-// console.log(getAllPropValues("category"));
-
-function getProductPrice(productName) {
+function getAllPropValues(propName) {
   const products = [
     { name: "Radar", price: 1300, quantity: 4 },
     { name: "Scanner", price: 2700, quantity: 3 },
     { name: "Droid", price: 400, quantity: 7 },
     { name: "Grip", price: 1200, quantity: 9 },
   ];
+
+  const array = [];
+
   for (const product of products) {
-    if (product.name === productName) {
-      return product ? product.price : null;
-    }
+    return array.push(product[propName]);
   }
 }
 
-console.log(getProductPrice("Radar"));
-console.log(getProductPrice("Engine"));
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("category"));
