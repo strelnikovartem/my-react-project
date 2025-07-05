14e134b44fcd516f356084b2e7f11f4e3cb32201
Artem Strelnikov <piramida1237@gmail.com>
2025-07-05 12:03:50 +0200

    sdfvb

--- src/task.js
+++ src/task.js
@@ -1,4 +1,4 @@
-function getAllPropValues(propName) {
+function calculateTotalPrice(productName) {
   const products = [
     { name: "Radar", price: 1300, quantity: 4 },
     { name: "Scanner", price: 2700, quantity: 3 },
@@ -6,17 +6,18 @@ function getAllPropValues(propName) {
     { name: "Grip", price: 1200, quantity: 9 },
   ];
 
-  const array = [];
+  let totalPrice = 0;
 
   for (const product of products) {
-    if (propName in product) {
-      array.push(product[propName]);
+    if (product.name !== productName) {
+      totalPrice = `Product <productName> not found!`;
     }
+    totalPrice = product.price * product.quantity;
   }
-  return array;
+  return totalPrice;
 }
 
-console.log(getAllPropValues("name"));
-console.log(getAllPropValues("quantity"));
-console.log(getAllPropValues("category"));
-console.log(getAllPropValues("price"));
+console.log(calculateTotalPrice("Blaster"));
+console.log(calculateTotalPrice("Radar"));
+console.log(calculateTotalPrice("Grip"));
+console.log(calculateTotalPrice("Droid"));
