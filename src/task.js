31e4d66c80c43fddd2cec2f93acf9900337c310c
Artem Strelnikov <piramida1237@gmail.com>
2025-07-06 20:37:05 +0200

    zxcv

--- src/task.js
+++ src/task.js
@@ -1,21 +1,8 @@
-function calculateTotalPrice(productName) {
-  const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
+const obj = {
+  method(value) {
+    console.log(`I'm a method with ${value}!`);
+  },
+};
 
-  for (const product of products) {
-    if (product.name === productName) {
-      return product.price * product.quantity;
-    }
-  }
-
-  return `Product ${productName} not found!`;
-}
-
-console.log(calculateTotalPrice("Radar"));
-console.log(calculateTotalPrice("Grip"));
-console.log(calculateTotalPrice("Droid"));
-console.log(calculateTotalPrice("Blaster"));
+obj.method(56); // "I'm a method with 5!"
+obj.method(10); // "I'm a method with 10!"
