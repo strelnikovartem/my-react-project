c29e3989e3dbb931fd6a4123bc76b9bce899e196
Artem Strelnikov <piramida1237@gmail.com>
2025-07-07 20:16:37 +0200

    lkjh

--- src/task.js
+++ src/task.js
@@ -1,18 +1,7 @@
-function multiply(...args) {
-  console.log(args);
+function multiply(fi, sec, ...args) {
+  console.log(fi, sec, args);
 }
 
-multiply(1, 2); // [1, 2]
-multiply(1, 2, 3); // [1, 2, 3]
-multiply(1, 2, 3, 4); // [1, 2, 3, 4]
-
-function add(...args) {
-  let sum = 0;
-  for (const item of args) {
-    sum += item;
-  }
-  return sum;
-}
-
-console.log(add(15, 27));
-console.log(add(12, 4, 11, 48));
+multiply(1, 2); // 1 2
+multiply(1, 2, 3); // 1 2 [3]
+multiply(1, 2, 3, 4); // 1 2 [3, 4]
