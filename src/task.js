e03f0bce292ce3518d088a910adf642e3a730377
Artem Strelnikov <piramida1237@gmail.com>
2025-07-07 21:55:32 +0200

    frgtfrgtf

--- src/task.js
+++ src/task.js
@@ -2,3 +2,20 @@ const first = { propA: 5, propB: 10 };
 const second = { propC: 15 };
 const third = { ...first, ...second };
 console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+
+const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+console.log(finalSettings);
