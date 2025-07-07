a6539917e4d6537e92844dcf2c86042009811ad8
Artem Strelnikov <piramida1237@gmail.com>
2025-07-07 20:12:59 +0200

    dcfv

--- src/task.js
+++ src/task.js
@@ -5,3 +5,14 @@ function multiply(...args) {
 multiply(1, 2); // [1, 2]
 multiply(1, 2, 3); // [1, 2, 3]
 multiply(1, 2, 3, 4); // [1, 2, 3, 4]
+
+function add(...args) {
+  let sum = 0;
+  for (const item of args) {
+    sum += item;
+  }
+  return sum;
+}
+
+console.log(add(15, 27));
+console.log(add(12, 4, 11, 48));
