65986795df1afbe38f0432a855abe25e64ae48e3
Artem Strelnikov <piramida1237@gmail.com>
2025-07-07 21:43:04 +0200

    cdfvfc

--- src/task.js
+++ src/task.js
@@ -1,15 +1,4 @@
-const firstGroupScores = [64, 42, 93];
-const secondGroupScores = [89, 14, 51, 26];
-const thirdGroupScores = [29, 47, 18, 97, 81];
-
-const allScores = [
-  ...firstGroupScores,
-  ...secondGroupScores,
-  ...thirdGroupScores,
-];
-const bestScore = Math.max(...allScores);
-const worstScore = Math.min(...allScores);
-
-console.log(allScores);
-console.log(bestScore);
-console.log(worstScore);
+const first = { propA: 5, propB: 10 };
+const second = { propC: 15 };
+const third = { ...first, ...second };
+console.log(third); // { propA: 5, propB: 10, propC: 15 }
