177beccf31170d483e7e8a1935a0f338d8ee0a2e
Artem Strelnikov <piramida1237@gmail.com>
2025-07-09 20:04:06 +0200

    bvn

--- src/task.js
+++ src/task.js
@@ -1,15 +1,14 @@
-function deliverPizza(pizzaName) {
-  return `Delivering ${pizzaName} pizza.`;
+function registerGuest(name, callback) {
+  console.log(`Registering ${name}!`);
+  callback(name);
 }
 
-function makePizza(pizzaName) {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
-}
+// Передаємо інлайн-функцію greet у якості колбека
+registerGuest("Mango", function greet(name) {
+  console.log(`Welcome ${name}!`);
+});
 
-function makeMessage(pizzaName, back) {
-  return back(pizzaName);
-}
-
-console.log(makeMessage("Royal Grand", makePizza));
-
-console.log(makeMessage("Ultracheese", deliverPizza));
+// Передаємо інлайн-функцію notify у якості колбека
+registerGuest("Poly", function notify(name) {
+  console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+});
