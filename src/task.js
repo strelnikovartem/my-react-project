65198818091db4485b1e32044363bbaba745fac9
Artem Strelnikov <piramida1237@gmail.com>
2025-07-09 22:02:30 +0200

    ggg

--- src/task.js
+++ src/task.js
@@ -1,39 +1,11 @@
-const numbers = [5, 10, 15, 20, 25];
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
 
-// Класичний for
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(`Index ${i}, value ${numbers[i]}`);
-}
-
-// Перебираючий метод forEach
-numbers.forEach(function (number, index) {
-  console.log(`Index ${index},`);
-});
-
-// function calculateTotalPrice(orderedItems) {
-//   let totalPrice = 0;
-
-//   orderedItems.forEach(function (n) {
-//     totalPrice += n;
-//   });
-
-//   return totalPrice;
-// }
-
-// console.log(calculateTotalPrice([12, 85, 37, 4]));
-
-function filterArray(numbers, value) {
-  let totalArray = [];
-
-  numbers.forEach(function (num) {
-    if (num > value) {
-      totalArray.push(num);
-    }
+  orderedItems.forEach(function (item) {
+    totalPrice += item;
   });
-  return totalArray;
+
+  return totalPrice;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(calculateTotalPrice([12, 85, 37, 4]));
