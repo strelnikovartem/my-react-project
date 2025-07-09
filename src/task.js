b5497180e1a5cf9f6fc2eb435933043232c11181
Artem Strelnikov <piramida1237@gmail.com>
2025-07-09 18:22:12 +0200

    bhvggg

--- src/task.js
+++ src/task.js
@@ -1,19 +1,18 @@
 function greet(name) {
-  return `Welcome ${name}!`;
+  console.log(`Welcome ${name}!`);
 }
 
-// Викликаємо функцію greet і виводимо результат у консоль
-console.log(greet("Mango")); // "Welcome Mango!"
-
-// Виводимо функцію greet у консоль, не викликаючи її
-console.log(greet); // ƒ greet() {return `Welcome ${name}!`}
-
-function makePizza() {
-  return "Your pizza is being prepared, please wait.";
+function notify(name) {
+  console.log(`Dear ${name}, your room will be ready in 30 minutes`);
 }
 
-const result = makePizza();
-const pointer = makePizza;
+function registerGuest(name, callback) {
+  console.log(`Registering ${name}!`);
+  callback(name);
+}
 
-console.log(result);
-console.log(pointer);
+registerGuest("Mango", greet); // "Registering Mango!"
+// "Welcome Mango!"
+
+registerGuest("Mango", notify); // "Registering Mango!"
+// "Dear Mango, your room will be ready in 30 minutes"
