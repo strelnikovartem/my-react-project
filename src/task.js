465fe00929079357b6b6710ec692c4b9b82f70fe
Artem Strelnikov <piramida1237@gmail.com>
2025-07-09 19:42:07 +0200

    xcv

--- src/task.js
+++ src/task.js
@@ -6,8 +6,8 @@ function makePizza(pizzaName) {
   return `Pizza ${pizzaName} is being prepared, please wait...`;
 }
 
-function makeMessage(pizzaName) {
-  return;
+function makeMessage(pizzaName, back) {
+  return back(pizzaName);
 }
 
 console.log(makeMessage("Royal Grand", makePizza));
