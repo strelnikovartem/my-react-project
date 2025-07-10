2ef98fd74b2f641ce8904e762a4a3e9be7e9dfe6
Artem Strelnikov <piramida1237@gmail.com>
2025-07-10 19:27:17 +0200

    cdfvc

--- src/task.js
+++ src/task.js
@@ -1,5 +1,23 @@
-const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
 
-const planetsLengths = planets.map((i) => i.length);
+const titles = books.map((book) => book.title);
 
-console.log(planetsLengths);
+console.log(titles);
