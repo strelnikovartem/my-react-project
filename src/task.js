3a3b5eeb6ff1d7417cfb4a45f25e5fedc9a2d065
Artem Strelnikov <piramida1237@gmail.com>
2025-07-10 18:02:09 +0200

    ggg

--- src/task.js
+++ src/task.js
@@ -1,33 +1,10 @@
-// const pureMultiply = (array, value) => {
-//   const newArray = [];
+const planets = ["Earth", "Mars", "Venus", "Jupiter"];
 
-//   array.forEach((element) => {
-//     newArray.push(element * value);
-//   });
+const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
+console.log(planetsInUpperCase); // ["EARTH", "MARS", "VENUS", "JUPITER"]
 
-//   return newArray;
-// };
+const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
+console.log(planetsInLowerCase); // ["earth", "mars", "venus", "jupiter"]
 
-// const numbers = [1, 2, 3, 4, 5];
-// const doubledNumbers = pureMultiply(numbers, 2);
-
-// // Мутація вихідних даних не відбулася
-// console.log(numbers); // [1, 2, 3, 4, 5]
-// // Функція повернула новий масив зі зміненими даними
-// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
-
-const changeEven = (numbers, value) => {
-  let total = [];
-
-  numbers.forEach((i) => {
-    if (i % 2 === 0) {
-      total.push(i + value);
-    } else {
-      total.push(i);
-    }
-  });
-  return total;
-};
-
-console.log(changeEven([1, 2, 3, 4, 5], 10));
-console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+// Оригінальний масив не змінився
+console.log(planets); // ["Earth", "Mars", "Venus", "Jupiter"]
