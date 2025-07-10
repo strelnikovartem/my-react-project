c501490634a2a17995f52a0e705683ff90476263
Artem Strelnikov <piramida1237@gmail.com>
2025-07-10 17:53:29 +0200

    dfgv

--- src/task.js
+++ src/task.js
@@ -1,11 +1,33 @@
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
 
-  orderedItems.forEach(function (item) {
-    totalPrice += item;
+//   array.forEach((element) => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers, 2);
+
+// // Мутація вихідних даних не відбулася
+// console.log(numbers); // [1, 2, 3, 4, 5]
+// // Функція повернула новий масив зі зміненими даними
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+const changeEven = (numbers, value) => {
+  let total = [];
+
+  numbers.forEach((i) => {
+    if (i % 2 === 0) {
+      total.push(i + value);
+    } else {
+      total.push(i);
+    }
   });
+  return total;
+};
 
-  return totalPrice;
-}
-
-console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(changeEven([1, 2, 3, 4, 5], 10));
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
