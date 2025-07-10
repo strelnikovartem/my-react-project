6005161fe24eabf316707b81e4dd1ee9f84c1aa5
Artem Strelnikov <piramida1237@gmail.com>
2025-07-10 20:16:21 +0200

    gggh

--- src/task.js
+++ src/task.js
@@ -1,24 +1,20 @@
-const values = [51, -3, 27, 21, -68, 42, -37];
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+  { name: "Houston", score: 64 },
+];
 
-const positiveValues = values.filter((value) => value >= 0);
-console.log(positiveValues); // [51, 27, 21, 42]
-// до positiveValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були >= 0
+const best = students.filter((student) => student.score >= HIGH_SCORE);
+console.log(best); // Масив об'єктів з іменами Mango і Kiwi
 
-const negativeValues = values.filter((value) => value < 0);
-console.log(negativeValues); // [-3, -68, -37]
-// до negativeValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були < 0
+const worst = students.filter((student) => student.score < LOW_SCORE);
+console.log(worst); // Масив з одним об'єктом Ajax
 
-const bigValues = values.filter((value) => value > 1000);
-console.log(bigValues); // []
-// до bigValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були > 1000
-
-console.log(values); // [51, -3, 27, 21, -68, 42, -37]
-// Оригінальний масив values не змінився
-
-const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
-
-const evenNumbers = numbers.filter((i) => i % 2 === 0);
-const oddNumbers = numbers.filter((i) => i % 2 !== 0);
-
-console.log(evenNumbers);
-console.log(oddNumbers);
+const average = students.filter(
+  (student) => student.score >= LOW_SCORE && student.score < HIGH_SCORE
+);
+console.log(average); // Масив об'єктів з іменами Poly і Houston
