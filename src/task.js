95f7f05efb9beb4c84fe7f2f15cbe5807400fa1b
Artem Strelnikov <piramida1237@gmail.com>
2025-07-10 20:14:55 +0200

    dfg

--- src/task.js
+++ src/task.js
@@ -1,85 +1,24 @@
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
+const values = [51, -3, 27, 21, -68, 42, -37];
 
-const getUserEmails = users.map((user) => {
-  return user.email;
-});
+const positiveValues = values.filter((value) => value >= 0);
+console.log(positiveValues); // [51, 27, 21, 42]
+// до positiveValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були >= 0
 
-console.log(getUserEmails);
+const negativeValues = values.filter((value) => value < 0);
+console.log(negativeValues); // [-3, -68, -37]
+// до negativeValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були < 0
+
+const bigValues = values.filter((value) => value > 1000);
+console.log(bigValues); // []
+// до bigValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були > 1000
+
+console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+// Оригінальний масив values не змінився
+
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+const evenNumbers = numbers.filter((i) => i % 2 === 0);
+const oddNumbers = numbers.filter((i) => i % 2 !== 0);
+
+console.log(evenNumbers);
+console.log(oddNumbers);
