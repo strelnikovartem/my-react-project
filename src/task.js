74293102689d56562e10f2368cfd0f7fc8116f6e
Artem Strelnikov <piramida1237@gmail.com>
2025-07-10 18:24:56 +0200

    dfcv

--- src/task.js
+++ src/task.js
@@ -1,10 +1,5 @@
 const planets = ["Earth", "Mars", "Venus", "Jupiter"];
 
-const planetsInUpperCase = planets.map((i) => i.toUpperCase());
-console.log(planetsInUpperCase); // ["EARTH", "MARS", "VENUS", "JUPITER"]
+const planetsLengths = planets.map((i) => i.length);
 
-const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
-console.log(planetsInLowerCase); // ["earth", "mars", "venus", "jupiter"]
-
-// Оригінальний масив не змінився
-console.log(planets); // ["Earth", "Mars", "Venus", "Jupiter"]
+console.log(planetsLengths);
