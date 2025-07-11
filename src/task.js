9647ff667c370e97728425de1827a92c00fc0519
Artem Strelnikov <piramida1237@gmail.com>
2025-07-11 22:32:05 +0200

    zxcz

--- src/task.js
+++ src/task.js
@@ -1,42 +1,14 @@
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    rating: 7.94,
-  },
-  {
-    title: "Enemy of God",
-    author: "Bernard Cornwell",
-    rating: 8.67,
-  },
+const students = [
+  { name: "Mango", score: 83, courses: ["mathematics", "physics"] },
+  { name: "Poly", score: 59, courses: ["science", "mathematics"] },
+  { name: "Ajax", score: 37, courses: ["physics", "biology"] },
+  { name: "Kiwi", score: 94, courses: ["literature", "science"] },
 ];
 
-const sortedByAuthorName = books.toSorted((a, b) =>
-  a.author.localeCompare(b.author)
-);
+const names = students
+  .toSorted((a, b) => a.score - b.score)
+  .map((student) => student.name);
 
-const sortedByReversedAuthorName = books.toSorted((a, b) =>
-  b.author.localeCompare(a.author)
-);
+console.log(names); // ["Ajax", "Poly", "Mango", "Kiwi"]
 
-const sortedByAscendingRating = books.toSorted((a, b) => a - b);
-
-const sortedByDescentingRating = books.toSorted((a, b) => b - a);
-
-console.log(sortedByAuthorName);
-console.log(sortedByReversedAuthorName);
+console.log(names); // ["Ajax", "Poly", "Mango", "Kiwi"]
