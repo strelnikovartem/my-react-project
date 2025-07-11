469450f8f718dfb0ea5b4d691d3cdabedfe091fd
Artem Strelnikov <piramida1237@gmail.com>
2025-07-11 21:41:10 +0200

    lmkn

--- src/task.js
+++ src/task.js
@@ -1,12 +1,5 @@
-const players = [
-  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-  { name: "Poly", playtime: 469, gamesPlayed: 2 },
-  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-];
+const scores = [61, 19, 74, 35, 92, 56];
+const ascendingScores = scores.toSorted();
 
-const totalAveragePlaytimePerGame = players.reduce((acc, player) => {
-  return acc + player.playtime / player.gamesPlayed;
-}, 0);
-
-console.log(totalAveragePlaytimePerGame);
+console.log(scores); // [61, 19, 74, 35, 92, 56]
+console.log(ascendingScores); // [19, 35, 56, 61, 74, 92]
